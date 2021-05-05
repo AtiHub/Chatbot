82b3348b80925d1a3d7f3eef311d4994e7efe711
AtiHub <atakanacar19999@hotmail.com>
2021-05-05 20:33:23 +0300

    stuff done May 5th

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { AskUsComponent } from './ask-us/ask-us.component';
+import { FaqComponent } from './faq/faq.component';
+import { MainComponent } from './main/main.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: MainComponent},
+  {path: 'faq', component: FaqComponent},
+  {path: 'askus', component: AskUsComponent},
+  {path: 'about', component: AboutComponent},
+  {path: '**', redirectTo: '/'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,24 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainComponent } from './main/main.component';
 import { HeaderComponent } from './header/header.component';
+import { FaqComponent } from './faq/faq.component';
+import { AskUsComponent } from './ask-us/ask-us.component';
+import { AboutComponent } from './about/about.component';
+import { CategoryComponent } from './main/category/category.component';
+import { SuggestionComponent } from './main/suggestion/suggestion.component';
+import { ChatbotComponent } from './main/chatbot/chatbot.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainComponent,
-    HeaderComponent
+    HeaderComponent,
+    FaqComponent,
+    AskUsComponent,
+    AboutComponent,
+    CategoryComponent,
+    SuggestionComponent,
+    ChatbotComponent
   ],
   imports: [
     BrowserModule,
